4e1ffb52366bb65382a55abdaf75e7cf4d2eb800
Kenichi Ukai <rise0646@gmail.com>
2018-12-09 14:18:05 +0900

    delete unnecessary files

--- node/app/sample_app/src/App.vue
+++ node/app/sample_app/src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
