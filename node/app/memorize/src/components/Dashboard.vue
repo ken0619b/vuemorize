85c7bf837c78278b2e9bb91d1bde0d1c4fa9f0dc
Kenichi Ukai <rise0646@gmail.com>
2018-12-28 11:41:17 +0900

    manage components depends on the client

--- node/app/memorize/src/components/Dashboard.vue
+++ node/app/memorize/src/components/Dashboard.vue
@@ -3,18 +3,20 @@
     <div>Dashboard</div>
     <h1>Hello {{ email }}!!</h1>
     <button @click="signOut">Sign out</button>
+    <p>登録しているカードを表示</p>
+    <div
+      v-for="(card, index) in cards"
+      :key="`${card.mondai}-${index}`"
+    >{{card.mondai}} - {{card.kotae}}</div>
     <div>
-      <p>カード追加</p>
-      <input type="text" placeholder="mondai" v-model="mondai">
-      <input type="text" placeholder="kotae" v-model="kotae">
-      <button @click="createCard">カードを追加</button>
-    </div>
-    <div>
-      <p>登録しているカードを表示</p>
-      <div
-        v-for="(card, index) in cards"
-        :key="`${card.mondai}-${index}`"
-      >{{card.mondai}} - {{card.kotae}}</div>
+      <div v-if="device == 'sp'">
+        <p>CardDeckコンポーネントから</p>
+        <CardDeck v-bind:cardData="cards"/>
+      </div>
+      <div v-if="device == 'pc'">
+        <p>CardAppendコンポーネントから</p>
+        <CardAppend/>
+      </div>
     </div>
   </div>
 </template>
@@ -24,12 +26,19 @@ import firebase from "firebase";
 import store from "@/store/index.js";
 import { mapGetters } from "vuex";
 
+import isMobile from "@/utils/DeviceUtil.js";
+import CardDeck from "@/components/CardDeck";
+import CardAppend from "@/components/CardAppend";
+
 export default {
+  components: {
+    CardDeck,
+    CardAppend
+  },
   name: "Dashboard",
   data() {
     return {
-      mondai: "",
-      kotae: ""
+      device: ""
     };
   },
   created() {
@@ -37,12 +46,19 @@ export default {
     // TODO async fetchが動かなかった原因
     // https://qiita.com/kurosame/items/6ab7622fe30c299a693e
     this.fetchData();
+
+    // // デバイス判定
+    if (isMobile.isMobile() || isMobile.isTablet()) {
+      this.device = "sp";
+    } else {
+      this.device = "pc";
+    }
   },
   computed: {
-    cards() {
+    cards: function() {
       return store.getters.getCards;
     },
-    email() {
+    email: function() {
       return store.getters.getEmail;
     }
   },
@@ -71,23 +87,6 @@ export default {
 
       // emailの初期化
       store.dispatch("setEmail", "");
-    },
-    createCard: function() {
-      if (this.mondai != "" && this.kotae != "") {
-        console.log(`${this.mondai} - ${this.kotae}`);
-        const newCard = {
-          mondai: this.mondai,
-          kotae: this.kotae
-        };
-
-        store.dispatch("createCard", newCard);
-
-        // 対象ユーザのカードを取得
-        store.dispatch("fetchCards");
-
-        this.mondai = "";
-        this.kotae = "";
-      }
     }
   }
 };
